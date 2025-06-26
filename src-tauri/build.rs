b29a3101cffec6bc3499839c2f339c89e57f01ec
Ethan Wang <avaniawang@outlook.com>
2025-06-26 21:56:13 +0800

    add proto with tauri

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
-fn main() {
-    tauri_build::build()
+use std::io::Result;
+
+fn main() -> Result<()> {
+    prost_build::compile_protos(&["proto/fileshare.proto"], &["proto/"])?;
+    tauri_build::build();
+
+    Ok(())
 }
